a448c9049c2224f0200604d840350d404f660eec
Nikita Dudnik <nikdudnik@gmail.com>
2023-01-13 20:02:33 +0100

    Dump

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,22 +1,61 @@
 import vert from './plasma.vert?raw'
 import frag from './plasma.frag?raw'
-import { Suspense, useMemo, useRef } from 'react'
+import { Suspense, useEffect, useMemo, useRef } from 'react'
 
 import { PerspectiveCamera, Stats, useAspect } from '@react-three/drei'
 
 import { useControls, folder } from 'leva'
 
 import { BlendFunction } from 'postprocessing'
-import { DepthOfField, EffectComposer, Noise, Scanline } from '@react-three/postprocessing'
+import {
+	DepthOfField,
+	EffectComposer,
+	Noise,
+	Scanline,
+} from '@react-three/postprocessing'
 import { ShaderMaterial } from 'three'
 extend({ ShaderMaterial })
 import { extend, useFrame, useThree } from '@react-three/fiber'
 
+import { el } from '@elemaudio/core'
+import WebRenderer from '@elemaudio/web-renderer'
+
+const ctx = new AudioContext()
+const core = new WebRenderer()
+
 const Scene = () => {
+	useEffect(() => {
+		; (async () => {
+			let node = await core.initialize(ctx, {
+				numberOfInputs: 0,
+				numberOfOutputs: 1,
+				outputChannelCount: [2],
+			})
+
+			node.connect(ctx.destination)
+			core.on('load', function () {
+				// Before actually rendering anything we put a click handler on the button so that
+				// this example doesn't start making noise automatically
+				window.addEventListener('click', async function (e) {
+					if (ctx.state === 'suspended') {
+						await ctx.resume()
+					}
+					core.render(
+						el.lowpass(
+							el.add(200, el.mul(1000, el.mul(0.5, el.add(1, el.cycle(0.02))))),
+							1.2,
+							el.mul(0.1, el.noise())
+						)
+					)
+				})
+			})
+		})()
+	}, [])
+
 	const sphereMaterialRef = useRef<ShaderMaterial>(null!)
 	const planeMaterialRef = useRef<ShaderMaterial>(null!)
 
-	const { viewport, gl } = useThree()
+	const { viewport } = useThree()
 
 	const scale = useAspect(viewport.width, viewport.height)
 
@@ -37,7 +76,7 @@ const Scene = () => {
 				value: 12 / 100,
 			},
 			uR: {
-				value: [20 / 100, 120 / 100, 19 / 100]
+				value: [20 / 100, 120 / 100, 19 / 100],
 			},
 			uFlip: { value: 1.0 },
 		}),
@@ -74,8 +113,8 @@ const Scene = () => {
 		FX: folder({
 			DOF: true,
 			Scanline: false,
-			Noise: true
-		})
+			Noise: true,
+		}),
 	}
 
 	const plasmaData = useControls(controls)
@@ -87,7 +126,11 @@ const Scene = () => {
 			sphereMaterial.uniforms.uTime.value = clock.getElapsedTime() / 5
 			sphereMaterial.uniforms.uAspectRatio.value = uAspectRatio
 			sphereMaterial.uniforms.uScale.value = plasmaData.Scale / 1000
-			sphereMaterial.uniforms.uR.value = [plasmaData.R1 / 100, plasmaData.R2 / 100, plasmaData.R3 / 100]
+			sphereMaterial.uniforms.uR.value = [
+				plasmaData.R1 / 100,
+				plasmaData.R2 / 100,
+				plasmaData.R3 / 100,
+			]
 			sphereMaterial.uniforms.uFlip.value = 1.0
 		}
 		if (planeMaterialRef.current) {
@@ -95,10 +138,13 @@ const Scene = () => {
 			planeMaterial.uniforms.uTime.value = clock.getElapsedTime() / 5
 			planeMaterial.uniforms.uAspectRatio.value = uAspectRatio
 			planeMaterial.uniforms.uScale.value = plasmaData.Scale / 1000
-			planeMaterial.uniforms.uR.value = [plasmaData.R1 / 100, plasmaData.R2 / 100, plasmaData.R3 / 100]
+			planeMaterial.uniforms.uR.value = [
+				plasmaData.R1 / 100,
+				plasmaData.R2 / 100,
+				plasmaData.R3 / 100,
+			]
 			planeMaterial.uniforms.uFlip.value = 1.0
 		}
-
 	})
 
 	return (
@@ -107,24 +153,45 @@ const Scene = () => {
 			<PerspectiveCamera makeDefault position={[0, 0, 5]} />
 			<mesh scale={1.5}>
 				<sphereGeometry args={[1, 128, 64]} />
-				<shaderMaterial ref={sphereMaterialRef} uniforms={uniforms} fragmentShader={frag} vertexShader={vert} />
+				<shaderMaterial
+					ref={sphereMaterialRef}
+					uniforms={uniforms}
+					fragmentShader={frag}
+					vertexShader={vert}
+				/>
 			</mesh>
 			<mesh scale={scale}>
 				<planeGeometry args={[1, 1]} />
-				<shaderMaterial ref={planeMaterialRef} uniforms={uniforms} fragmentShader={frag} vertexShader={vert} />
+				<shaderMaterial
+					ref={planeMaterialRef}
+					uniforms={uniforms}
+					fragmentShader={frag}
+					vertexShader={vert}
+				/>
 			</mesh>
 			<EffectComposer>
-				{plasmaData.DOF ? <DepthOfField focusDistance={4} focalLength={0.2} bokehScale={2} height={1024} /> : <></>}
-				{plasmaData.Scanline ? <Scanline
-					blendFunction={BlendFunction.OVERLAY} // blend mode
-					density={1.25} // scanline density
-				/> : <></>}
+				{plasmaData.DOF ? (
+					<DepthOfField
+						focusDistance={4}
+						focalLength={0.2}
+						bokehScale={2}
+						height={1024}
+					/>
+				) : (
+					<></>
+				)}
+				{plasmaData.Scanline ? (
+					<Scanline
+						blendFunction={BlendFunction.OVERLAY} // blend mode
+						density={1.25} // scanline density
+					/>
+				) : (
+					<></>
+				)}
 				{plasmaData.Noise ? <Noise opacity={0.7} /> : <></>}
 			</EffectComposer>
 		</Suspense>
-
 	)
 }
 
 export default Scene
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot, events, extend } from '@react-three/fiber'
 import { StrictMode } from 'react'
-import { Group, Mesh, PerspectiveCamera, PlaneGeometry, SphereGeometry } from 'three'
 
-extend({ SphereGeometry, PlaneGeometry, Mesh, PerspectiveCamera, Group })
+import { Group, Mesh, PerspectiveCamera, PlaneGeometry, SphereGeometry } from 'three'
+extend({ Group, Mesh, PerspectiveCamera, PlaneGeometry, SphereGeometry })
 
 import Scene from './Scene'
 import './main.css'
@@ -10,7 +10,7 @@ import './main.css'
 const root = createRoot(document.querySelector('canvas')!)
 
 window.addEventListener('resize', () => {
-	root.configure({ events, camera: { position: [0, 0, 50] }, size: { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight } })
+	root.configure({ events, size: { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight } })
 	root.render(<StrictMode><Scene /></StrictMode>)
 })
 
